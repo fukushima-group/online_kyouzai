0a20629861385304f757368d1ccde1c3d57badb5
“yuevo” <yuya.nishimura@di-v.co.jp>
2020-10-05 17:39:02 +0900

    ログインリンク作成のみ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :students
   devise_for :teachers
-  root to: "items#index"
+  root to: "exams#index"
   resources :exams do
     # resources :transactions, only: [:index, :new, :create]
   end
