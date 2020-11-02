b1ffeffba5eb3ce5472e98f0b2bed8dc5b063f7d
“yuevo” <yuya.nishimura@di-v.co.jp>
2020-11-02 21:21:31 +0900

    messageモデル追加

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -4,7 +4,7 @@ class TestsController < ApplicationController
 
   def index
     @exam = Exam.find(params[:exam_id])
-    @tests = Test.where(exam_id: params[:exam_id])
+    @tests = Test.where(exam_id: params[:exam_id]).sample(5)
   end
 
   def new
